11d3fb57bde0c70acc8e3b206c629b176af9ab9e
Nataliia Selianska <natari.sev@gmail.com>
2023-10-10 17:32:56 +0200

    finish step 1

--- README.md
+++ README.md
@@ -5,4 +5,3 @@
 ### CodeClimate:
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/0d6517ef93191e6df05e/maintainability)](https://codeclimate.com/github/ShanyAilurus/frontend-project-11/maintainability)
-[![Test Coverage](https://api.codeclimate.com/v1/badges/0d6517ef93191e6df05e/test_coverage)](https://codeclimate.com/github/ShanyAilurus/frontend-project-11/test_coverage)
--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,15 @@ const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
-  entry: './src/index.js',
+  entry: './src/js/main.js',
   output: {
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    open: true,
-    host: 'localhost',
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -21,27 +23,30 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader',
           },
-        },
-      },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [require('autoprefixer')],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
       },
     ],
   },
@@ -50,7 +55,6 @@ const config = {
 module.exports = () => {
   if (isProduction) {
     config.mode = 'production';
-
     config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
   } else {
     config.mode = 'development';
